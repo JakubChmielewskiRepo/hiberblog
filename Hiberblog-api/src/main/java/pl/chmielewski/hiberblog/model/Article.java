964f01276bbc1ad366f96d557ed295c50af25dba
Jakub Chmielewski <j.chmielewski1008@gmail.com>
2020-11-26 21:13:15 +0100

    Submiting api on git

---
+++ Hiberblog-api/src/main/java/pl/chmielewski/hiberblog/model/Article.java
@@ -0,0 +1,59 @@
+package pl.chmielewski.hiberblog.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+
+@Entity
+public class Article {
+
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int articleId;
+  private String title;
+  private String text;
+
+  public Article() {
+  }
+
+  public Article( String title, String text) {
+    this.title = title;
+    this.text = text;
+  }
+
+  public int getArticleId() {
+    return articleId;
+  }
+
+  public void setArticleId(int articleId) {
+    this.articleId = articleId;
+  }
+
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  @Override
+  public String toString() {
+    return "Article{" +
+            "articleId=" + articleId +
+            ", title='" + title + '\'' +
+            ", text='" + text + '\'' +
+            '}';
+  }
+}
---
+++ Hiberblog-api/src/main/java/pl/chmielewski/hiberblog/model/HelpId.java
@@ -0,0 +1,13 @@
+package pl.chmielewski.hiberblog.model;
+
+public class HelpId {
+    private int articleId;
+
+    public int getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(int articleId) {
+        this.articleId = articleId;
+    }
+}
